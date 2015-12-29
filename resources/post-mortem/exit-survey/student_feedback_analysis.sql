04bf157a295778ef00e8435efe825a3af3418207
MJ Rossetti (@s2t2) <s2t2mail+git@gmail.com>
2015-12-29 17:27:24 -0500

    consolodating exit responses into one table

--- resources/post-mortem/exit-survey/student_feedback_analysis.sql
+++ resources/post-mortem/exit-survey/student_feedback_analysis.sql
@@ -19,11 +19,7 @@ RETURN CASE
   WHEN s = "Never heard of" THEN 0
 END;
 
-/*
-
-ORIGINAL RESPONSES
-
-*/
+/* ORIGINAL ONBOARDING RESPONSES */
 
 DROP TABLE IF EXISTS badm_2301._responses;
 CREATE TABLE badm_2301._responses as (
@@ -78,61 +74,26 @@ CREATE TABLE badm_2301._responses as (
 ALTER TABLE badm_2301._responses ADD PRIMARY KEY(student_id);
 SELECT * FROM badm_2301._responses;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 /*
 
 EXIT RESPONSES
 
 */
 
-
-/* EVALUATIONS OF THE COURSE AND INSTRUCTORS AND LEARNING */
-
-DROP TABLE IF EXISTS badm_2301.xr_evals_learning;
-CREATE TABLE badm_2301.xr_evals_learning AS (
+DROP TABLE IF EXISTS badm_2301._exit_responses;
+CREATE TABLE badm_2301._exit_responses AS (
   SELECT
-    student_id
-    ,agreement_response_to_int(obj_subjectmatter) as subject_matter -- Learning Objectives [I expanded my subject matter knowledge]
-    ,agreement_response_to_int(obj_techskills) as tech_skills -- Learning Objectives [I improved my technology skills]
-    ,agreement_response_to_int(obj_industry) as industry_insights -- Learning Objectives [I learned more about contemporary industry practices and challenges]
-    ,agreement_response_to_int(obj_creativity) as tech_creativity -- Learning Objectives [I leveraged technology as an outlet for creativity]
-    ,agreement_response_to_int(obj_pres) as presentation_comms -- Learning Objectives [I improved my communication and presentation skills]
-    ,agreement_response_to_int(obj_writing) as writing -- Learning Objectives [I improved my writing skills]
-    ,agreement_response_to_int(obj_teamwork) as teamwork -- Learning Objectives [I improved my ability to work in teams]
-  FROM badm_2301.exit_responses
-);
-ALTER TABLE badm_2301.xr_evals_learning ADD PRIMARY KEY(student_id);
-SELECT * FROM badm_2301.xr_evals_learning;
+    g.student_id
+    ,g.final_grade
+    ,g.final_letter_grade
+    ,agreement_response_to_int(obj_subjectmatter) AS subject_matter -- Learning Objectives [I expanded my subject matter knowledge]
+    ,agreement_response_to_int(obj_techskills) AS tech_skills -- Learning Objectives [I improved my technology skills]
+    ,agreement_response_to_int(obj_industry) AS industry_insights -- Learning Objectives [I learned more about contemporary industry practices and challenges]
+    ,agreement_response_to_int(obj_creativity) AS tech_creativity -- Learning Objectives [I leveraged technology as an outlet for creativity]
+    ,agreement_response_to_int(obj_pres) AS presentation_comms -- Learning Objectives [I improved my communication and presentation skills]
+    ,agreement_response_to_int(obj_writing) AS writing -- Learning Objectives [I improved my writing skills]
+    ,agreement_response_to_int(obj_teamwork) AS teamwork -- Learning Objectives [I improved my ability to work in teams]
 
-DROP TABLE IF EXISTS badm_2301.xr_evals_course;
-CREATE TABLE badm_2301.xr_evals_course AS (
-  SELECT
-    student_id
     ,agreement_response_to_int(lectures_relevant) AS lectures_relevant -- Course Evaluation [Lectures are relevant]
     ,agreement_response_to_int(lectures_engaging) AS lectures_engaging -- Course Evaluation [Lectures are engaging]
     ,agreement_response_to_int(lectures_fun) AS lectures_fun -- Course Evaluation [Lectures are fun]
@@ -146,15 +107,7 @@ CREATE TABLE badm_2301.xr_evals_course AS (
     ,agreement_response_to_int(project_engaging) AS project_engaging -- Course Evaluation [Group Project is engaging]
     ,agreement_response_to_int(project_fun) AS project_fun -- Course Evaluation [Group Project is fun]
     ,agreement_response_to_int(project_expectclear) AS project_expectclear -- Course Evaluation [Group Project expectations are clear]
-  FROM badm_2301.exit_responses_course
-);
-ALTER TABLE badm_2301.xr_evals_course ADD PRIMARY KEY(student_id);
-SELECT * FROM badm_2301.xr_evals_course;
 
-DROP TABLE IF EXISTS badm_2301.xr_evals_instructor;
-CREATE TABLE badm_2301.xr_evals_instructor AS (
-  SELECT
-    student_id
     ,ta_rec -- Teaching Assistant Recommendation
     ,prof_rec -- Professor Recommendation
     ,agreement_response_to_int(prof_caresmysuccess) AS prof_caresmysuccess -- Professor Evaluation [Prof cares about my success]
@@ -169,34 +122,7 @@ CREATE TABLE badm_2301.xr_evals_instructor AS (
     ,agreement_response_to_int(prof_highexpectations) AS prof_highexpectations -- Professor Evaluation [Prof has high expectations]
     ,agreement_response_to_int(prof_wiseclasstime) AS prof_wiseclasstime -- Professor Evaluation [Prof makes wise use of class time]
     ,agreement_response_to_int(self_comfcommswprof) AS prof_comfcomms -- Professor Evaluation [I am comfortable communicating with the Prof]
-  FROM badm_2301.exit_responses_instruct
-);
-ALTER TABLE badm_2301.xr_evals_instructor ADD PRIMARY KEY(student_id);
-SELECT * FROM badm_2301.xr_evals_instructor;
 
-/* COMFORTABILITY WITH TOOLS AND SOFTWARE */
-
-DROP TABLE IF EXISTS badm_2301.xr_evals_lang;
-CREATE TABLE badm_2301.xr_evals_lang AS (
-  SELECT
-    student_id
-    ,comfort_response_to_int(data_csv) as data_csv
-    ,comfort_response_to_int(data_json) as data_json
-    ,comfort_response_to_int(data_xml) as data_xml
-    ,comfort_response_to_int(lang_sql) as lang_sql
-    ,comfort_response_to_int(lang_html) as lang_html
-    ,comfort_response_to_int(lang_css) as lang_css
-    ,comfort_response_to_int(lang_js) as lang_js
-  FROM badm_2301.exit_responses_lang
-);
-ALTER TABLE badm_2301.xr_evals_lang ADD PRIMARY KEY(student_id);
-SELECT * FROM badm_2301.xr_evals_lang;
-
-
-DROP TABLE IF EXISTS badm_2301.xr_evals_lang;
-CREATE TABLE badm_2301.xr_evals_lang AS (
-  SELECT
-    student_id
     ,comfort_response_to_int(data_csv) AS data_csv
     ,comfort_response_to_int(data_json) AS data_json
     ,comfort_response_to_int(data_xml) AS data_xml
@@ -204,15 +130,7 @@ CREATE TABLE badm_2301.xr_evals_lang AS (
     ,comfort_response_to_int(lang_html) AS lang_html
     ,comfort_response_to_int(lang_css) AS lang_css
     ,comfort_response_to_int(lang_js) AS lang_js
-  FROM badm_2301.exit_responses_lang
-);
-ALTER TABLE badm_2301.xr_evals_lang ADD PRIMARY KEY(student_id);
-SELECT * FROM badm_2301.xr_evals_lang;
 
-DROP TABLE IF EXISTS badm_2301.xr_evals_soft;
-CREATE TABLE badm_2301.xr_evals_soft AS (
-  SELECT
-    student_id
     ,comfort_response_to_int(soft_word) AS soft_word
     ,comfort_response_to_int(soft_gdocs) AS soft_gdocs
     ,comfort_response_to_int(soft_excel) AS soft_excel
@@ -225,20 +143,19 @@ CREATE TABLE badm_2301.xr_evals_soft AS (
     ,comfort_response_to_int(soft_atom) AS soft_atom
     ,comfort_response_to_int(soft_nppp) AS soft_nppp
     ,comfort_response_to_int(soft_ghdesktop) AS soft_ghdesktop
-  FROM badm_2301.exit_responses_soft
-);
-ALTER TABLE badm_2301.exit_responses_soft ADD PRIMARY KEY(student_id);
-SELECT * FROM badm_2301.exit_responses_soft;
 
-DROP TABLE IF EXISTS badm_2301.xr_evals_tools;
-CREATE TABLE badm_2301.xr_evals_tools AS (
-  SELECT
-    student_id
     ,comfort_response_to_int(comms_email) AS comms_email
     ,comfort_response_to_int(comms_bboard) AS comms_bboard
     ,comfort_response_to_int(comms_slack) AS comms_slack
     ,comfort_response_to_int(tools_gh) AS tools_gh
-  FROM badm_2301.exit_responses_tools
+
+  FROM badm_2301.gradebook g
+  LEFT JOIN badm_2301.exit_responses xr ON g.student_id = xr.student_id
+  LEFT JOIN badm_2301.exit_responses_course xc ON g.student_id = xc.student_id
+  LEFT JOIN badm_2301.exit_responses_instruct xp ON g.student_id = xp.student_id
+  LEFT JOIN badm_2301.exit_responses_lang xl ON g.student_id = xl.student_id
+  LEFT JOIN badm_2301.exit_responses_soft xs ON g.student_id = xs.student_id
+  LEFT JOIN badm_2301.exit_responses_tools xt ON g.student_id = xt.student_id
 );
-ALTER TABLE badm_2301.xr_evals_tools ADD PRIMARY KEY(student_id);
-SELECT * FROM badm_2301.xr_evals_tools;
+ALTER TABLE badm_2301._exit_responses ADD PRIMARY KEY(student_id);
+SELECT * FROM badm_2301._exit_responses;
